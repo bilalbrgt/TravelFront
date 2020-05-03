59efb1ff47210498892fa1aad2ae887dd1953aa6
bilalbrgt <boujemaouibilel@gmail.com>
2020-05-03 23:06:31 +0200

    navabr

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Footer from "../Footer";
 import "../Css/navbar.css";
+
 function Navbar() {
+  const [Largeur, setLargeur] = useState(window.innerWidth);
+  const [togglemenu, setToogleMenu] = useState(false);
+  const toogleNavbar = () => {
+    setToogleMenu(!togglemenu);
+  };
+
+  useEffect(() => {
+    const changewidth = () => {
+      setLargeur(window.innerWidth);
+      if (window.innerWidth > 500) {
+        setToogleMenu(false);
+      }
+    };
+    window.addEventListener("resize", changewidth);
+    return () => {
+      window.removeEventListener("resize", changewidth);
+    };
+  }, []);
   return (
     <div className="nav">
       <header>
@@ -15,22 +34,40 @@ function Navbar() {
           </Link>
 
           <nav>
-            <ul>
-              <li>
-                <Link to="/">Accueil </Link>
-              </li>
-              <li>
-                <Link to="/destinations"> Destinations </Link>
-              </li>
+            {(Largeur > 500 || togglemenu) && (
+              <div className="liste">
+                <ul>
+                  <li>
+                    <Link to="/" className="items">
+                      Accueil{" "}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/destinations" className="items">
+                      {" "}
+                      Destinations{" "}
+                    </Link>
+                  </li>
 
-              <li>
-                <Link to="/login"> Compte </Link>
-              </li>
-              <li>
-                <Link to="/contact"> Contact </Link>
-              </li>
-            </ul>
+                  <li>
+                    <Link to="/login" className="items">
+                      {" "}
+                      Compte{" "}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/contact" className="items">
+                      {" "}
+                      Contact{" "}
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </nav>
+          <div className="btn" onClick={toogleNavbar}>
+            <i class="fas fa-sliders-h"></i>
+          </div>
         </div>
       </header>
       <section></section>
